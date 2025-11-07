fe36c6e7f4d43b37cb6c0828f56b8cd7aa1cdf1f
Finbyz <info@finbyz.tech>
2025-11-07 10:45:32 +0000

    (ai) layout added

---
+++ src/app/(ai)/layout.tsx
@@ -0,0 +1,25 @@
+import Footer from "@/components/layout/footer";
+import Header from "@/components/layout/header";
+import InquiryForm from "@/components/ui/InquiryForm";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default async function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+    <>
+      <Header />
+      <main>
+        {children}
+        <InquiryForm />
+      </main>
+      <Footer />
+    </>
+  );
+}
